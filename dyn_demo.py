b0becbc1ae3cb6dc317e22ad32e072e949d1fc17
Jim Mei <jimmei20010628@gmail.com>
2023-12-01 15:25:07 -0500

    12.1 Afternoon Test

--- dyn_demo.py
+++ dyn_demo.py
@@ -18,7 +18,7 @@ from lib.calcAngDiff import calcAngDiff
 import lib.IK_position_null
 import Helper_function
 import Frame_Trans
-import Basic_action
+from Basic_action import Gripper_control
 import Dynamic_Basic_action
 
 import numpy as np
@@ -47,8 +47,8 @@ if __name__ == "__main__":
     input("\nWaiting for start... Press ENTER to begin!\n") # get set!
     print("Go!\n") # go!
     
-    seed = start_position
-    dynamic_pre_grab_seed = np.array([-1.18062,  0.73451, -0.55257, -1.33372,  0.39019,  1.96088,  0.591 ]) # Dyn Grab Pos
+    # seed = start_position
+    # dynamic_pre_grab_seed = np.array([-1.18062,  0.73451, -0.55257, -1.33372,  0.39019,  1.96088,  0.591 ]) 
     Place_seed=np.array([-0.12021102-0.3 , 0.20148171 ,-0.17911063 ,-2.02175873 , 0.0447598 ,  2.21961924,0.46256746])
     # NB_seed = np.array([0,0,0,-pi/2,0,pi/2,pi/4])
 
@@ -63,9 +63,12 @@ if __name__ == "__main__":
                             [0, 0, 0, 1]])
 
     # Pre_grab_pos = np.array([-1.12638,  1.36176-0.25, -0.51741, -0.58107, 0.18119,  2.7103,   1.13345])
-    Pre_grab_pos = np.array([-1.05623,  1.02066, -0.57354, -0.9058,   0.41996,  2.91373,  0.90335]) # 12.1
-    # Pre_place_pos = np.array([-0.01779206-0.3, -0.76012354,  0.01978261, -2.34205014, 0.02984053, 1.54119353, 0.75344866])
-    Pre_place_pos = np.array([-1.08441,  1.25679, -0.51034, -0.90505,  0.45643,  3.15141,  0.91422]) #12.1
+    
+    ####################################################################################### VERY IMPORTANT! ######################################
+    Pre_grab_pos = np.array([-2.24349895,  0.69896332,  0.45332957, -1.5018697,  -2.30542716,  3.74539539, -0.81328175]) # 12.1 ##################
+    grab_pos = np.array([-2.10535123,  1.01677975,  0.50319099, -1.16210212, -2.12033052,  3.55645052, -0.92487665])# 12.1 #######################
+    ##############################################################################################################################################
+    Pre_place_pos = np.array([-0.01779206-0.3, -0.76012354,  0.01978261, -2.34205014, 0.02984053, 1.54119353, 0.75344866])
     
 
     FK=FK()
@@ -104,7 +107,7 @@ if __name__ == "__main__":
 
         # test = is_block_grabbed(arm)
         while not rospy.is_shutdown(): # IMPORTANT: The loop condition might need to be changed!
-            arm.close_gripper()
+            Gripper_control(arm, "close")
             print("loop grip closed")
             # Check if the block is grabbed
             if is_block_grabbed(arm):
@@ -116,7 +119,8 @@ if __name__ == "__main__":
                 break
             else:
                 print("Grab unsuccessful, retrying...")
-            arm.open_gripper()
+            # arm.open_gripper()
+            Gripper_control(arm, "open")
             print("loop grip opened!")
         arm.safe_move_to_position(Pre_grab_pos) ### Move to position above the pre-grab!
         Stacked_Layers += 1
